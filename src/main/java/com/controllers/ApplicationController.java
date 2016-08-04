53b0e463c764a0dfa9c7ee792060499e1a7dd977
pratik <pratik@grid-scape.com>
2016-08-04 18:20:30 +0530

    ehardware

---
+++ src/main/java/com/controllers/ApplicationController.java
@@ -0,0 +1,10 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller	
+@RequestMapping("/application")
+public class ApplicationController {
+
+}
